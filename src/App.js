0ac8f0c4365a4fa935a7b47ab777a484fdc1eda1
Irina Soboleva <iralsob@gmail.com>
2023-01-20 17:41:00 +0100

    add meals list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
+import AvailableMeals from "./components/Meal/AvailableMeals";
+import MealsSummary from "./components/Meal/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+        <MealsSummary />
+        <AvailableMeals />
+      </main>
     </Fragment>
   );
 }
