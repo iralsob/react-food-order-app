028fafbe3b5a395447444859241967c9f6d97f88
Irina Soboleva <iralsob@gmail.com>
2023-01-21 19:20:35 +0100

    add button animation with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
@@ -6,10 +6,27 @@ import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
   const numberOfAddedItems = cartCtx.items.reduce((currNum, item) => {
-    return(currNum+item.amount);
+    return currNum + item.amount;
   }, 0);
+
+  const [isBtnAnimated, setIsBtnAnimated] = useState(false);
+
+  const classesBtn = `${classes.button} ${isBtnAnimated ? classes.bump : ""}`;
+ 
+  useEffect(() => {
+    setIsBtnAnimated(true);
+    
+    const timer = setTimeout(() => {
+      setIsBtnAnimated(false);
+    }, 300);
+    
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={classesBtn}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
